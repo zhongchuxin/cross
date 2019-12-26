4e23940f309cc95a1f6f422bb35c3035fa914720
ZhongChuxin <1152853086@qq.com>
2019-12-26 16:09:31 +0800

    12.26

---
+++ build/precache-manifest.f84298bfe66b9f5c652cdf0ddf644b2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4df5275e06919e9e5980ae138829c5f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26d065bf5a8b67ecc4a8",
+    "url": "./static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "8a83808158163cc96714",
+    "url": "./static/js/2.a7f526f7.chunk.js"
+  },
+  {
+    "revision": "26d065bf5a8b67ecc4a8",
+    "url": "./static/js/main.dd2d25c2.chunk.js"
+  },
+  {
+    "revision": "56a6b2399647f3d207bd",
+    "url": "./static/js/runtime-main.f1c4a8af.js"
+  }
+]);
